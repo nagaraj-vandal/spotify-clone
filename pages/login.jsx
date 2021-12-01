578063f238133733d16790bda93744a2b75d6246
nagaraj-vandal <nagarajvandal@gmail.com>
2021-12-01 22:40:01 +0530

    login page css changes

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,10 +4,10 @@ import {getProviders , signIn} from "next-auth/react"
 function Login({providers}) {
     console.log(Object.keys(providers))
     return (
-        <div>
+        <div className="flex items-center flex-col min-h-screen bg-black w-full justify-center">
            <img src="https://links.papareact.com/9xl" alt="" className="w-52 mb-5" />
            {Object.values(providers).map((provider) => (
-               <button>Login with {provider.name}</button>
+               <button className="bg-[#18D860] p-4 text-white rounded-lg   ">Login with {provider.name}</button>
            ))}
         </div>
     )
