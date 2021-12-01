24845cbd343108adc92429a2b1a49aa4c22cfbb7
nagaraj-vandal <nagarajvandal@gmail.com>
2021-12-01 23:06:51 +0530

    login changes

--- pages/login.jsx
+++ pages/login.jsx
@@ -7,7 +7,10 @@ function Login({providers}) {
         <div className="flex items-center flex-col min-h-screen bg-black w-full justify-center">
            <img src="https://links.papareact.com/9xl" alt="" className="w-52 mb-5" />
            {Object.values(providers).map((provider) => (
-               <button className="bg-[#18D860] p-4 text-white rounded-lg   ">Login with {provider.name}</button>
+               <button 
+               className="bg-[#18D860] p-4 text-white rounded-lg"
+               onClick={() => signIn(provider.id , {callbackUrl : "/"})}
+               >Login with {provider.name}</button>
            ))}
         </div>
     )
